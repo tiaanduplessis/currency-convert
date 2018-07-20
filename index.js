1e442184b95b82067925136fa0c323b7ccc927b5
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2018-07-20 08:15:43 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-'use strict'
-
-const assert = require('assert')
-
 const req = require('puny-req')
 const money = require('money')
+const type = require('@tiaanduplessis/type')
 
-const url = 'https://api.fixer.io/latest'
+const url = 'https://data.fixer.io/api/latest?access_key='
 
 let data
+
+function assert (cond, msg = '') {
+  if (!cond) {
+    throw new Error(msg)
+  }
+}
 
 /**
  * Convert amount from one currency to another
  *
+ * @param {String} apiKey fixer.io API key
  * @param {Number} amount Amount to convert
  * @param {String} from Currency code to convert from e.g. 'USD', 'HKD' or 'ZAR'
  * @param {String} to Currency code to convert to e.g. 'USD', 'HKD' or 'ZAR'
  */
-const currencyConvert = (amount = 1, from = '', to = '') => {
+const currencyConvert = (apiKey = '', amount = 1, from = '', to = '') => {
   assert(
-    typeof amount === 'number',
+    type.isNum(amount),
     'currency-convert: Amount should be number'
   )
-  assert(typeof from === 'string', 'currency-convert: From should be string')
-  assert(typeof to === 'string', 'currency-convert: To should be string')
+  assert(type.isStr(from), 'currency-convert: From should be string')
+  assert(type.isStr(to), 'currency-convert: To should be string')
 
   from = from.toUpperCase()
   to = to.toUpperCase()
@@ -39,7 +43,8 @@ const currencyConvert = (amount = 1, from = '', to = '') => {
     )
   }
 
-  return req(url).then(json => {
+  return req(`${url}${apiKey}`)
+  .then(json => {
     data = json.body
     money.base = data.base
     money.rates = data.rates
